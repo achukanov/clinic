a854565cebe55ebded98b784dee8f3d9179d739c
QQQ <a.chukanov@rambler.ru>
2023-02-06 13:24:03 +0300

    совместил branch и add_question

--- clinic/urls.py
+++ clinic/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('medicine', views.medicine, name='medicine'),
     path('medicine/<slug:slug>', views.branch, name='branch'),
     path('medicine/<slug:slug>/<int:id>', views.doctor, name='doctor'),
-    path('add-question', views.add_question, name='add-question'),
+    # path('add-question', views.add_question, name='add-question'),
     path('prices', views.price, name='price'),
     path('laborators', views.laborators, name='laborators'),
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Doctors, Specializations, Price, Certificates, Questions
 from custom.models import IndexSlider
 from custom.models import Maps
-from django.http import HttpResponse, HttpResponseRedirect
 from .forms import QuestionForm
-from django import forms
-
-
-# TODO: request, title, description, phone(+7(988)8604300), phone_title(+7(988)860-43-00),
-# TODO: mail(andros-008@mail.ru)
-# TODO: booking(https://booking.medflex.ru/?user=d2264a28a42691145544d9fea59cf0c9)
 
 
 def index(request):
@@ -64,6 +57,12 @@ def laborators(request):
 
 
 def branch(request, slug):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('medicine')
+
     spec = Specializations.objects.filter(slug=slug).first()
     doctors = Doctors.objects.filter(specialization=spec).filter(active=True)
     certificates = Certificates.objects.filter(doctor__in=doctors, active=True).order_by('-sorting')
@@ -82,20 +81,13 @@ def branch(request, slug):
                   })
 
 
-# TODO: совместить branch и add_question
-def add_question(request):
-    if request.method == 'POST':
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            form.save()
-    return redirect('medicine')
-
-
 def doctor(request, slug, id):
     doc = Doctors.objects.filter(pk=int(id)).first()
+    video = 'url youtube link'
     print(id, doc)
     return render(request,
                   'clinic/doctor.html', {
                       'request': request,
-                      'doc': doc
+                      'doc': doc,
+                      'video': video
                   })
